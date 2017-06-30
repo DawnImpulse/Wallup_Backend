39f42831b03053fbb503b6afd857ef1517a4e525
Saksham <saksham@stonevire.com>
2017-06-30 16:16:04 +0530

    Improvement : List of New Images Signed-off-by: Saksham <saksham@stonevire.com>

--- v1/source/listNewImages.js
+++ v1/source/listNewImages.js
@@ -1,6 +1,7 @@
-var sqlQuery        = 'Select aid,details,favouriteCount,downloadCount,viewCount,tags,tagged from images ORDER BY aid DESC LIMIT ?';
+var sqlQuery        = 'Select aid,details,favouriteCount,downloadCount,viewCount,tags from images '+
+                         'WHERE tagged=1 ORDER BY aid DESC LIMIT ?';
 
-var pageNo,
+var page_no,
     lowerLimit,
     upperLimit,
     sqlQueryParams,
@@ -9,42 +10,45 @@ var pageNo,
     
 w1.on('w1fetch', function (req,res) {
 		
-    pageNo          = req.body.pageNo,
+    page_no          = req.body.page_no,
     
-    upperLimit = pageNo*10;
+    upperLimit = page_no*10;
     lowerLimit = upperLimit - 10;
     
     //Querying DB for list of images
     sql_conn.query(sqlQuery,upperLimit,function(err,result){
-            if(err){
+            if(err)
+            {
                     console.log(err);
                     file.appendFile(errorLogFile,dateTime+" :: "+err+ "\n" ,function(err){console.log(err);});                
                     res.json({success      : "false",
                               errorID      : "listNewImages.w1fetch.query",
                               errorMessage : "Error In Querying Database" }); //Response                
-                }else{                    
-                    if(upperLimit>result.length)
-                        {
-                            upperLimit = result.length;
-                        }
+            }else
+            {                    
+                if(upperLimit>result.length)
+                    {
+                        upperLimit = result.length;
+                    }
 
-                    for(var i=lowerLimit;i<upperLimit;i++)  //Creating Image Json Object and pushing to Array
-                        {
-                            imagesJson = {aid : result[i].aid,
-                                          details : result[i].details,
-                                          favouriteCount : result[i].favouriteCount,
-                                          downloadCount : result[i].downloadCount,
-                                          viewCount : result[i].viewCount,
-                                          tags : result[i].tags};
-                            responseArray.push(imagesJson);
-                        }
-                        
-                    res.json({success    : "true",
-                              message    : "Successfully Data Returned",
-                              images     : responseArray}); //Response
+                for(var i=lowerLimit;i<upperLimit;i++)  //Creating Image Json Object and pushing to Array
+                    {
+                        imagesJson = {aid : result[i].aid,
+                                      details : result[i].details,
+                                      favouriteCount : result[i].favouriteCount,
+                                      downloadCount : result[i].downloadCount,
+                                      viewCount : result[i].viewCount,
+                                      tags : result[i].tags };
 
-                    responseArray = [];
-                }
+                        responseArray.push(imagesJson);
+                    }
+                    
+                res.json({success    : "true",
+                            message    : "Successfully Data Returned",
+                            images     : responseArray}); //Response
+
+                responseArray = [];
+            }
         
         }); //end of sqlConn.query
     });//end of w1.on
